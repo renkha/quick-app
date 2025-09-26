72c6ee62237ab662c75a38f482ee334e74b71569
renodiandika <renodiandika@refactory.id>
2025-09-27 06:36:42 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ 
+        message: 'Hello Docker!', 
+        container: process.env.HOSTNAME 
+    });
+});
+
+app.listen(3000, () => console.log('App running on port 3000'));
